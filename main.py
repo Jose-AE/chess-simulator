eddb7bb743cb79edfc78c816fdd9e58b72e8eadc
Gatuno7000 <66221090+Gatuno7000@users.noreply.github.com>
2022-09-29 15:59:39 -0500

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Screen
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600,600))
+
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+
+    pygame.display.update() 
